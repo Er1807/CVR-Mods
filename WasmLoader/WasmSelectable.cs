664ce78c8559cef602a38428913cc5fd3a7dfe3b
Eric van Fandenfart <20169013+Er1807@users.noreply.github.com>
2022-08-16 21:00:58 +0200

    Create WasmSelectable.cs

---
+++ WasmLoader/WasmSelectable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WasmSelectable : MonoBehaviour
+{
+    public bool AllowWorldScripts;
+    public bool AllowAvatarScripts;
+    public string[] AllowedTypes;
+}
