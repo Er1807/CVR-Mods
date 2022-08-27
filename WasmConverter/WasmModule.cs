554241c0ecc51a7366abe45f91a1c7c85d875e81
Eric van Fandenfart <20169013+Er1807@users.noreply.github.com>
2022-08-27 12:19:25 +0200

    Cleanup

--- WasmConverter/WasmModule.cs
+++ WasmConverter/WasmModule.cs
@@ -28,56 +28,6 @@ namespace Converter
             foreach (var function in Functions.SelectMany(x => x.Instructions).Where(x => x.Instruction == WasmInstructions.call && x.Operand is WasmExternFunctionOperand).Select(x => x.Operand as WasmExternFunctionOperand).GroupBy(x => x.FunctionName).Select(x => x.First()))
             {
                 builder.AppendLine($@"  (import ""env"" ""{function.FunctionName}"" (func ${function.FunctionName} {WasmFunction.BuildParamString(function.Params.Select(x => Converter.GetWasmType(x).Value).ToList(), true)} {WasmFunction.BuildResultString(Converter.GetWasmType(function.ReturnValue.FullName))}))");
-
-                /*if (func is MethodDef method)
-                {
-                    if (method.Name == ".ctor")
-                    {
-                        var namectr = WasmInstruction.ConvertMethod(method.DeclaringType.FullName, method.Name, false, method.GetParams(), method.ReturnType ?? method?.DeclaringType.ToTypeSig());
-                        builder.AppendLine($"  (import \"env\" \"{namectr}\"(func ${namectr} {WasmFunction.BuildParamString(method.GetParams().Select(x => Converter.GetWasmType(x).Value).ToList(), true)} {WasmFunction.BuildResultString(Converter.GetWasmType(method.DeclaringType.FullName))}))");
-                        continue;
-                    }
-                    var name = WasmInstruction.ConvertMethod(method.DeclaringType.FullName, method.Name, method.HasThis, method.GetParams(), method.ReturnType);
-                    builder.AppendLine($"  (import \"env\" \"{name}\"(func ${name} {WasmFunction.BuildParamString(method.GetParams().Select(x => Converter.GetWasmType(x).Value).ToList(), true)} {WasmFunction.BuildResultString(Converter.GetWasmType(method.ReturnType))}))");
-                }
-                if (func is MemberRef member)
-                {
-                    var name = "";
-                    if(member.Name == ".ctor")
-                    {
-                        name = WasmInstruction.ConvertMethod(member.Class.FullName, member.Name, false, member.GetParams(), member.ReturnType ?? member?.FieldSig.Type);
-                        builder.AppendLine($"  (import \"env\" \"{name}\"(func ${name} {WasmFunction.BuildParamString(member.GetParams().Select(x => Converter.GetWasmType(x).Value).ToList(), true)} {WasmFunction.BuildResultString(Converter.GetWasmType(member.DeclaringType.FullName))}))");
-                    }
-                    else if (member.IsFieldRef)
-                    {
-                        name = WasmInstruction.ConvertMethod(member.Class.FullName, member.Name, member.HasThis, member.GetParams(), member.ReturnType ?? member?.FieldSig.Type);
-                        builder.AppendLine($"  (import \"env\" \"{name}\"(func ${name} {WasmFunction.BuildParamString(new List<WasmDataType>() { WasmDataType.i32 }, true, member.HasThis)} {WasmFunction.BuildResultString(Converter.GetWasmType(member.ReturnType ?? member.FieldSig?.Type))}))");
-                    }
-                    else
-                    {
-                        name = WasmInstruction.ConvertMethod(member.Class.FullName, member.Name, member.HasThis, member.GetParams(), member.ReturnType ?? member?.FieldSig.Type);
-                        builder.AppendLine($"  (import \"env\" \"{name}\"(func ${name} {WasmFunction.BuildParamString(member.GetParams().Select(x => Converter.GetWasmType(x).Value).ToList(), true, member.HasThis)} {WasmFunction.BuildResultString(Converter.GetWasmType(member.ReturnType ?? member.FieldSig?.Type))}))");
-                    }
-                    
-                    
-                }
-
-                if (func is MethodDef method)
-                {
-                    if (method.Name == ".ctor")
-                    {
-                        var namectr = WasmInstruction.ConvertMethod(method.DeclaringType.FullName, method.Name, false, method.GetParams(), method.ReturnType ?? method?.DeclaringType.ToTypeSig());
-                        builder.AppendLine($"  (import \"env\" \"{namectr}\"(func ${namectr} {WasmFunction.BuildParamString(method.GetParams().Select(x => Converter.GetWasmType(x).Value).ToList(), true)} {WasmFunction.BuildResultString(Converter.GetWasmType(method.DeclaringType.FullName))}))");
-                        continue;
-                    }
-                    var name = WasmInstruction.ConvertMethod(method.DeclaringType.FullName, method.Name, method.HasThis, method.GetParams(), method.ReturnType);
-                    builder.AppendLine($"  (import \"env\" \"{name}\"(func ${name} {WasmFunction.BuildParamString(method.GetParams().Select(x => Converter.GetWasmType(x).Value).ToList(), true)} {WasmFunction.BuildResultString(Converter.GetWasmType(method.ReturnType))}))");
-                }
-                if (func is string str)
-                {
-                    
-                    builder.AppendLine($"  (import \"env\" \"{str}\"(func ${str} (result i32)))");
-                }*/
             }
 
             foreach (var field in Fields)
