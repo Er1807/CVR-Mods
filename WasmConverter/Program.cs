246b30fb8bcfa2dbacd919bf88e0ccddc9a28d46
Eric van Fandenfart <ruhlanderic@gmail.com>
2023-01-02 15:59:43 +0100

    Beginnings for C# -> IL -> modified IL -> C# -> C -> Wasm
    Why do I still try

--- WasmConverter/Program.cs
+++ WasmConverter/Program.cs
@@ -1,9 +1,13 @@
 ﻿#if UNITY_EDITOR
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Windows;
 using dnlib.DotNet;
+using dnlib.DotNet.Emit;
+using dnlib.DotNet.Writer;
 
 namespace Converter
 {
@@ -21,6 +25,80 @@ namespace Converter
             ModuleContext modCtx = ModuleDef.CreateModuleContext();
             ModuleDef module = ModuleDefMD.Load(args[0], modCtx);
             var type = module.Types.SingleOrDefault(x => x.FullName == args[1]);
+
+            for (int i = 0; i < module.Types.Count; i++)
+            {
+                if(type != module.Types[i])
+                {
+                    module.Types.Remove(module.Types[i]);
+                    i--;
+                }
+            }
+            var systemConsole = module.CorLibTypes.GetTypeRef("", "DontUseThisClass");
+            var writeLine2 =  new MemberRefUser(module, "Arr_Length",
+                            MethodSig.CreateStatic(module.CorLibTypes.Int32, module.CorLibTypes.Object),
+                            systemConsole);
+
+            List<ITypeDefOrRef> toRename = new List<ITypeDefOrRef>();
+
+            foreach (var method in type.Methods.Where(x => !x.IsConstructor))
+            {
+                foreach (var item in method.Body.Instructions)
+                {
+                    if (item.OpCode == OpCodes.Callvirt || item.OpCode == OpCodes.Call)
+                    {
+                        var refs = (item.Operand as MemberRef);
+                        var refs2 = (item.Operand as MethodSpec);
+                        var refs3 = (item.Operand as MethodDef);
+                        if (refs != null && !refs.Name.Contains("__"))
+                        {
+                            if (refs.MethodSig.HasThis)
+                            {
+                                refs.MethodSig.HasThis = false;
+                                refs.MethodSig.Params.Insert(0, refs.DeclaringType.ToTypeSig());
+                            }
+                            //item.OpCode = OpCodes.Call;
+                            refs.Name = refs.DeclaringType.Name + "__" + refs.Name;
+                            toRename.Add(refs.DeclaringType);
+
+                        }
+                        if (refs2 != null && !refs2.Name.Contains("__"))
+                        {
+                            //item.OpCode = OpCodes.Call;
+                            refs2.Name = refs2.DeclaringType.Name + "__" + refs2.Name;
+                            toRename.Add(refs2.DeclaringType);
+
+                        }
+                        if (refs3 != null && !refs3.Name.Contains("__"))
+                        {
+                            if (refs3.MethodSig.HasThis)
+                            {
+                                refs3.MethodSig.HasThis = false;
+                                refs3.MethodSig.Params.Insert(0, refs3.DeclaringType.ToTypeSig());
+                            }
+                            //item.OpCode = OpCodes.Call;
+                            refs3.Name = refs3.DeclaringType.Name + "__" + refs3.Name;
+                            toRename.Add(refs3.DeclaringType);
+
+                        }
+                    }
+                    if (item.OpCode == OpCodes.Ldlen)
+                    {
+                        item.OpCode = OpCodes.Call;
+                        item.Operand = writeLine2;
+                    }
+                }
+            }
+
+            foreach (var item in toRename)
+            {
+
+                item.Name = "DontUseThisClass";
+            }
+
+            module.Write("E:\\Temp\\test.dll");
+
+            return;
             TypeType = new CorLibTypes(module).GetTypeRef("System", "Type");
             TypeInt = new CorLibTypes(module).GetTypeRef("System", "Int32");
             TypeLong = new CorLibTypes(module).GetTypeRef("System", "Int64");
