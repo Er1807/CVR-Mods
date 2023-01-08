61ddc98045a0b190ba30e577396b4bdc5ff951de
Eric van Fandenfart <ruhlanderic@gmail.com>
2023-01-08 16:45:01 +0100

    Generate Runnable Code

--- WasmConverter/Program.cs
+++ WasmConverter/Program.cs
@@ -17,21 +17,66 @@ namespace Converter
 {
     internal class Program
     {
+        private const string DnSpyConsole = "C:\\Users\\Eric\\Downloads\\dnSpy-v6.2.0-64bit\\dnSpy.Console.exe";
+
         static void Main(string[] args)
         {
             ILPreparer.PrepareClass(args[0], args[1], "E:\\Temp\\test.dll");
+            string methods = GenerateCode(args[1], "E:\\Temp\\test.dll");
+            var cleanMethods = CleanupCode(methods);
+            string signatures = ExtractSignatures(cleanMethods);
 
+            Console.WriteLine("// Header");
+            Console.WriteLine("#include \"header.h\"");
+            Console.WriteLine();
+            Console.WriteLine("// Signatures");
+            Console.WriteLine(signatures);
+            Console.WriteLine("// Methods");
+            Console.WriteLine(cleanMethods);
+            return;
 
+        }
+
+        private static string ExtractSignatures(string methods)
+        {
+            var matchers = Regex.Matches(methods, "\\w+ \\w+ \\w+\\(.*?\\)");
+
+            StringBuilder builder = new StringBuilder();
+
+            for (int i = 0; i < matchers.Count; i++)
+            {
+                builder.AppendLine(matchers[i].Value + ";");
+
+            }
+
+            var signatures = builder.ToString();
+            return signatures;
+        }
+
+        private static string CleanupCode(string methods)
+        {
+            //Cleanup
+            methods = methods.Replace("this.", "");
+            methods = methods.Replace("DontUseThisClass.", "");
+            methods = methods.Replace("using ", "// using ");
+            methods = Regex.Replace(methods, " \\d+f", m => m.Groups[0].Value.Replace("f", ""));
+            methods = Regex.Replace(methods, "public class ([A-z]+) .*?{", m => $"//Class {m.Groups[1].Value}", RegexOptions.Singleline);
+            methods = Regex.Replace(methods, "}[\r\n ]*$", m => $"// End Class", RegexOptions.Singleline);
+            return methods;
+        }
+
+        private static string GenerateCode(string type, string dll)
+        {
             // Use ProcessStartInfo class
             ProcessStartInfo startInfo = new ProcessStartInfo();
             startInfo.CreateNoWindow = false;
             //startInfo.UseShellExecute = false;
-            startInfo.FileName = "C:\\Users\\Eric\\Downloads\\dnSpy-v6.2.0-64bit\\dnSpy.Console.exe";
+            startInfo.FileName = DnSpyConsole;
             startInfo.WindowStyle = ProcessWindowStyle.Maximized;
             startInfo.UseShellExecute = false;
             startInfo.RedirectStandardOutput = true;
             //startInfo.RedirectStandardError = true;
-            startInfo.Arguments= $"-t {args[1]} --member-order fpmet --no-color E:\\Temp\\test.dll";
+            startInfo.Arguments = $"-t {type} --member-order fpmet --no-color {dll}";
             StringBuilder builder = new StringBuilder();
             try
             {
@@ -46,15 +91,11 @@ namespace Converter
             }
             catch (Exception ex)
             {
-                // Log error.
+                Console.Error.WriteLine(ex);
+                throw;
             }
             var str = builder.ToString();
-
-            str = str.Replace("this.", "").Replace("DontUseThisClass.", "");
-            var replaced = Regex.Replace(str, " \\d+f", m => m.Groups[0].Value.Replace("f", ""));
-            Console.WriteLine(replaced);
-            return;
-
+            return str;
         }
     }
 }
